05856624feb2e411bebd455e628ebbd38a87ddbe
init-adina <adina@init.red>
2024-07-12 16:11:44 +0500

    changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./*{html,js}"],
-<<<<<<< HEAD
 	
 	theme: {
 		 container: {
@@ -17,25 +16,5 @@ export default {
 		},
   },
   plugins: [],
-=======
-	theme: {
-		extend: {},
-		content: ["./*{html,js}"],
 	
-		theme: {
-			container: {
-				padding: {
-					DEFAULT: "1rem",
-				},
-			},
-			extend: {
-				colors: {
-					'peach': '#FE9676',
-					'pink-custom': '#ED8792'
-				}
-			},
-		},
-		plugins: [],
-	}
->>>>>>> 97864b33842e72c4a0cb97c3b85bff2079eb80a2
 }
