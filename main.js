2c77d218ee334368af0718f1d6315b924c7a40dd
init-adina <adina@init.red>
2024-07-12 17:33:59 +0500

    made a burger

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+console.log("loaded")
+
+ const initBurger = () => {
+    const burgerBtn = document.querySelector('#burger-btn');
+    const menu = document.querySelector('#burger-menu');
+
+    if (!burgerBtn || !menu) return;
+
+	const toggleBurger = () => {
+			
+		console.log("here")
+        menu.classList.toggle("hidden");
+        burgerBtn.classList.toggle("opened");
+        document.documentElement.classList.toggle("overflow-hidden");
+    };
+
+    menu.addEventListener('click', (e) => {
+        if (!(e.target instanceof Element)) return;
+
+        if (!e.target.closest('.close-burger-link')) return;
+
+        toggleBurger();
+    });
+
+    burgerBtn.addEventListener("click", toggleBurger);
+};
 
 
-
+document.addEventListener('DOMContentLoaded', () => {
+	initBurger();
+})
